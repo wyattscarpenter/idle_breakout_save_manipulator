0be080c96115a375bd672fc492021d2516155fad
wyattscarpenter <wyattscarpenter@gmail.com>
2025-04-16 16:34:37 -0700

    code that changes the active level to 420

--- ibsm.py
+++ ibsm.py
@@ -1,21 +1,34 @@
-from sys import argv
+#!/usr/bin/env python3
+
+from sys import argv, stderr
 from base64 import b64decode as de, b64encode as en
+
+
+DEBUG = False
+
+def dprint(*args, **kwargs):
+    if DEBUG:
+        print(*args, file=stderr, **kwargs)
+
 def main(save_code: str):
-    print(save_code)
+    print()
+    dprint(save_code)
     with open('save_code.tmp.txt', 'w') as f:
         f.write(save_code)
     d = de(save_code)
     with open('save_code_d.tmp.txt', 'wb') as f:
         f.write(d)
     d = d.decode()
-    print(d)
+    dprint(d)
     sp = d.split(',')
-    print(sp)
+    dprint(sp)
+    sp[0] = "420"
     joined = ','.join(sp)
     e = joined.encode()
-    e = en(e)
-    print(e.decode()==save_code)
-    with open('save_code_e.tmp.txt', 'wb') as f:
+    e = en(e).decode()
+    dprint(e==save_code)
+    print(e)
+    with open('save_code_e.tmp.txt', 'w') as f:
         f.write(e)
 
 if __name__ == "__main__":
