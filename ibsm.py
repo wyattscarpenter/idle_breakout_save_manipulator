7551bbf4e8d700ab140adcb9c6cf124175ce7fa8
wyattscarpenter <wyattscarpenter@gmail.com>
2025-04-19 13:35:07 -0700

    increase gold

--- ibsm.py
+++ ibsm.py
@@ -11,7 +11,7 @@ def dprint(*args, **kwargs):
     if DEBUG:
         print(*args, file=stderr, **kwargs)
 
-save_key = ["level", "money"]
+save_key = ["level", "money", "gold", "gold at next reset"]
 
 def main(save_code: str):
     print()
@@ -25,8 +25,10 @@ def main(save_code: str):
     dprint(d)
     sp = d.split(',')
     dprint(sp)
+    x = lambda s: str(float(s)*1_000_000_000)
     sp[0] = "420"
-    sp[1] = str(float(sp[1])*1_000_000_000)
+    sp[1] = x(sp[1])
+    sp[2] = x(sp[2])
     sp[-5] = str(float(sp[-5])*1_000_000_000) # this is skill points
     for key, value in zip_longest(save_key, sp):
         print(f"{key or "???"}: {float(value) if value != '' else 0:,}")
